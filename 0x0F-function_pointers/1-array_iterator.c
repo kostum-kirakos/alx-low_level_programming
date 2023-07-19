dcc697b7e677eee677cbbfb73428d6a62c72d2e6
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-07-19 16:12:36 -0700

    function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,11 +12,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	int i;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
