60202cc42bff91e7537bb7efd0026cc637017e04
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-07-19 16:33:28 -0700

    function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,16 +12,15 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == NULL || size <= 0 || action == NULL)
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
-
-	unsigned int i;
-
 	for (i = 0; i < size; i++)
 	{
-		if (action(array[i]))
+		if (cmp(array[i]))
 		{
 			return (i);
 		}
